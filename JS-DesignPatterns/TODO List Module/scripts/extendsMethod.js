16ec669a94bfe8b69159d4cd48777ab2b306f385
Nick Angelov <nangelov1990@gmail.com>
2015-03-24 17:00:20 +0200

    TODO List Module: Base file creation

---
+++ JS-DesignPatterns/TODO List Module/scripts/extendsMethod.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+Function.prototype.extends = function(parent) {
+    this.prototype = Object.create(parent.prototype);
+    this.prototype.constructor = this;
+    this._super = parent;
+}
